e23f9e2d55519111e48677f7f958061dd5495a81
Richard <zhan97richard@gmail.com>
2015-05-12 11:10:52 -0400

    update

--- animation.c
+++ animation.c
@@ -7,6 +7,45 @@ t = 0 -> 1
 vary x 0 .5 0 100 (half the distance in 100 frames)
 
 
+-----------------------------------
+Animation in MDL
 
+move 100 300 0 fred #fred is a knob (will start at 0 -> 1)
+
+Basic Animation MDL Keywords:
+FRAMES n //sets up how many frames you're drawing
+
+BASENAME s - set the basname of each generated frame
+
+vary knob start_frame end_frame start_value end_value
+vary fred 0 9 0 1
+set the knob value for each fram of the animation
+
+STEP 1
+Look for the frames command (determine if animation is necessary)
+Look for the basename command
+Set frames and basename if possible
+Look for vary command - return an error if frames is not present
+One approach: (First two commands must be Frames + Basename if animation)
+
+STEP 2
+Handle each vary command - look through the list except for varys
+Symbol table can only hold one value per knob
+
+We must build a seperate structure for each value
+- consider an array in which each index represents a frame
+vary k 0 10 0 1
+vary x 3 6 1 0
+[k=0,k=.1,k=.2,k=.3,k=.4,k=.5,k=.6,k=.7,k=.8,k=.9,k=1]
+[x=1,x=1, x=1,,x=1,x=2/3,x=1/3,x=0.......]
+linked list works well :D
+
+STEP 3
+Generate image(s)
+If there is no animation code, generate a single image normally
+else loop through commands once per frame
+  - set value for each knob in the symbol table (step 2)
+  - go through commands, apply knobs when needed
+  - save basename+frame.png
 
  */
--- compiler.c
+++ compiler.c
@@ -21,4 +21,65 @@ IDENT x
 Parser - Performs syntax analysis. Knows the structure of the language. Generates a syntax tree from the token list (only syntax, if you declare a double as an int, it will pass through)
 
 Semantic Analyzer - go through the syntax tree to find errors in the meanigs of the words. Creates a symbol table containg all the identifiers from the tree and their values
+- returns a list of instructions in the order they need to be performed
+- instructions will have links to the symbol table
+
+Optimizer - optimizes (might change instructions)
+Goes through the list of instructions and may change/combine them to make the resulting code more efficient
+
+C compiler tools
+----------------
+lex - creates a lexer if given a definition of all possible tokens
+yacc - create a parse and semantic analyzer given the structure of the language (yet another compiler compiler)
+flex - bison
+
+
+source code -> compiler -> image(s)
+
+MDL - Motion Description Language
+
+\-?\.[0-9]+ { yylval.val = atof(yytest);return DOUBLE; }
+"//".* { return COMMENT; }
+"torus" { return TORUS; }
+"sphere" { return SPHERE; }
+"box" { return BOX; }
+"line" { return LINE; }
+
+
+
 */
+SYMTAB *s; //symbol table
+struct command op[MAX_COMMANDS]; //the list of commands
+
+//Bison Declarations
+
+%union{
+  double val;
+  char string[255];
+}
+
+%token COMMENT
+%token <val> DOUBLE
+%token <string> SPHERE TORUS BOX LINE CS MESH TEXTURE
+%token <string> STRING
+%%
+
+ //Grammar rules
+
+input:
+| input command
+;
+
+command:
+COMMENT {}|
+LIGHT STRING DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE DOUBLE
+
+MOVE DOUBLE DOUBLE DOUBLE STRING
+{
+  lineno++;
+  op[lastop].opcode = MOVE;
+  op[lastop].op.move.d[0] = $2; //dollar is which parameter
+  op[lastop].op.move.p = add_symbol($5,SYM_VALUE,0);
+  lastop++;
+}
+|
